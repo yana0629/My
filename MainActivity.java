de059926b4737d001977e035b75e66b0254a468d
yana0629 <129178823+yana0629@users.noreply.github.com>
2023-04-06 18:08:16 +0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,29 +1,81 @@
 package org.geeksforgeeks.my;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button searchButton;
     private TextView result;
+    private LinearLayout log;
+    private EditText first;
+    private EditText second;
+    private EditText third;
+    private Bundle savedInstanceState;
 
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
         searchButton = findViewById(R.id.button);
         result = findViewById(R.id.result);
+        log = findViewById(R.id.lala);
+        first = findViewById(R.id.first);
+        second = findViewById(R.id.second);
+        third = findViewById(R.id.third);
 
-        View.OnClickListener onClickListener = view -> result.setText("кнопка была нажата");
 
+        View.OnClickListener onClickListener = view -> {
+            result.setText("кнопка была нажата");
+        };
         searchButton.setOnClickListener(onClickListener);
+
+    }
+
+    String[] countries = { "2", "8", "10", "16"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.savedInstanceState = savedInstanceState;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView selection = findViewById(R.id.selection);
+
+        Spinner spinner = findViewById(R.id.second);
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, countries);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+
+                String item = (String) parent.getItemAtPosition(position);
+                selection.setText(item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        };
+        spinner.setOnItemSelectedListener(itemSelectedListener);
+
     }
 }
